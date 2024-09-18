d1702a81ef23063fb5a4691d659cdfa58c38ef39
cristianSierraE <sierraequihua@gmail.com>
2024-09-18 14:28:43 -0700

    Create ConsultaProlog.java

---
+++ ConsultaProlog.java
@@ -0,0 +1,10 @@
+package clue;
+import org.jpl7.Query;
+
+public class ConsultaProlog {
+    public static void main(String[] args) {
+        // Establecer la consulta Prolog
+        CLUE c = new CLUE();
+        c.setVisible(true);
+    }
+}
